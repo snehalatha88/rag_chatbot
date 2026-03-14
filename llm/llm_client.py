508df229cc83df0473747a092d08177374d2747c
Katuri Sneha Latha <katuri.latha@agivant.com>
2026-03-14 18:04:43 +0530

    Fix LLM connection using HuggingFace API

--- llm/llm_client.py
+++ llm/llm_client.py
@@ -1,17 +1,4 @@
-import requests
 import os
-
-API_URL = "https://api-inference.huggingface.co/models/google/flan-t5-base"
-
-HF_TOKEN = os.getenv("HF_TOKEN")
-
-headers = {"Authorization": f"Bearer {HF_TOKEN}"}
-
-
-def generate_answer(prompt):
-
-    payload = {
-        "inputs": prompt,import os
 import requests
 
 API_URL = "https://api-inference.huggingface.co/models/google/flan-t5-base"
@@ -33,16 +20,8 @@ def generate_answer(prompt):
 
     result = response.json()
 
-    # Handle possible API errors
+    # handle API errors
     if isinstance(result, dict) and "error" in result:
         return "Model is loading or API error occurred. Please try again."
-
-    return result[0]["generated_text"]
-        "parameters": {"max_new_tokens": 200}
-    }
-
-    response = requests.post(API_URL, headers=headers, json=payload)
-
-    result = response.json()
 
     return result[0]["generated_text"]
